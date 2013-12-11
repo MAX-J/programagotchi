29cc485b0cae5ca25190196e58cd6287a0065333
JosephTalbot <jt13692@my.bris.ac.uk>
2013-12-11 16:59:11 +0000

    Extra Variables
    More characters recognised by SDL

--- display/display.c
+++ display/display.c
@@ -3,10 +3,6 @@
 #include <string.h>
 #include "neillsdl2.h"
 
-#define Blu 51, 204, 255
-#define Grn 51, 255, 102
-#define Red 255, 10, 51
-
 #define HEIGHT 70
 #define WIDTH 110
 
@@ -16,19 +12,19 @@ int SDL (char board[][WIDTH]);
 
 int main (void){
 
-  	char board[HEIGHT][WIDTH];
+    char board[HEIGHT][WIDTH];
 
-  	for (int w = 0; w <WIDTH; w++){
-    	for (int h = 0; h <HEIGHT; h++){	
-      	board[h][w] = '.';
-    	}
-  	}
+    for (int w = 0; w <WIDTH; w++){
+      for (int h = 0; h <HEIGHT; h++){  
+        board[h][w] = '.';
+      }
+    }
 
-	for(int i=0; i<6; i++){
+  for(int i=0; i<6; i++){
   board[69-i][52+i] = '#';
-  board[69][52+i] = '#';
+  board[69][52+i] = 'y';
   }
-  	
+    
   for(int i=0; i<6; i++){
   board[68][36+i] = '.';
   board[68][36+i+1] = 'G';
@@ -36,7 +32,7 @@ int main (void){
 
   SDL(board);
 
-	return 0;
+  return 0;
 }
 
 int SDL (char board[][WIDTH]){
@@ -44,65 +40,95 @@ int SDL (char board[][WIDTH]){
   SDL_Simplewin sw;
 
   /* Initialisation*/
-	Neill_SDL_Init(&sw);
+  Neill_SDL_Init(&sw);
 
-	int SQH = (WWIDTH/WIDTH);
-	SDL_Rect rectangle;
+  int SQH = (WWIDTH/WIDTH);
+  SDL_Rect rectangle;
   rectangle.w = SQH;
   rectangle.h = SQH;
 
   do{
     for (int w = 0; w <WIDTH; w++){
-			for (int h = 0; h <HEIGHT; h++){
-		
-			switch(board[h][w]){
+      for (int h = 0; h <HEIGHT; h++){
+    
+      switch(board[h][w]){
 
-				case '#':
-				Neill_SDL_SetDrawColour(&sw, 134, 24, 123);
-      	rectangle.x = (w*SQH);
-      	rectangle.y = (h*SQH);
-      	SDL_RenderDrawRect(sw.renderer, &rectangle);
-				break;
+        /*
+        G Gotchi
+        1 Barrier
+        C Candy(each one get 10 score, when you get all of them,100,win)
+        S Hazard
+        E Exit
+        . Free space
+        # Border/wall
+        Default Non-descript item
+        */
 
-				case 'G':
-				Neill_SDL_SetDrawColour(&sw, 245, 2, 56);
-        for (int i = 0; i<10; i++){
-      	rectangle.x = (w*SQH+i);
-      	rectangle.y = (h*SQH);
-      	SDL_RenderFillRect(sw.renderer, &rectangle);
-        }
-				break;
+        case '1':
+        Neill_SDL_SetDrawColour(&sw, 134, 24, 123);
+        rectangle.x = (w*SQH);
+        rectangle.y = (h*SQH);
+        SDL_RenderDrawRect(sw.renderer, &rectangle);
+        break;
 
-				case '.':
-				Neill_SDL_SetDrawColour(&sw, 0, 0, 0);
-      	rectangle.x = (w*SQH);
-      	rectangle.y = (h*SQH);
-      	SDL_RenderFillRect(sw.renderer, &rectangle);
-				break;
+        case '#':
+        Neill_SDL_SetDrawColour(&sw, 245, 251, 255);
+        rectangle.x = (w*SQH);
+        rectangle.y = (h*SQH);
+        SDL_RenderDrawRect(sw.renderer, &rectangle);
+        break;
 
-				default:
-				Neill_SDL_SetDrawColour(&sw, 25, 135, 201);
-      	rectangle.x = (w*SQH);
-      	rectangle.y = (h*SQH);
-      	SDL_RenderFillRect(sw.renderer, &rectangle);
-				break;
-			}
-		}
-	}
-	// Updates window - no graphics appear on some devices until this is finished
-	SDL_RenderPresent(sw.renderer);
-	SDL_UpdateWindowSurface(sw.win);
-	// Checks if anyone has triggered the SDL window to end
-	Neill_SDL_Events(&sw);
-	}while(!sw.finished);
+        case 'G':
+        Neill_SDL_SetDrawColour(&sw, 114, 148, 56);
+        rectangle.x = (w*SQH);
+        rectangle.y = (h*SQH);
+        SDL_RenderFillRect(sw.renderer, &rectangle);
+        break;
 
-	Neill_SDL_SetDrawColour(&sw, 0, 0, 0);
+        case 'S':
+        Neill_SDL_SetDrawColour(&sw, 245, 2, 56);
+        rectangle.x = (w*SQH);
+        rectangle.y = (h*SQH);
+        SDL_RenderFillRect(sw.renderer, &rectangle);
+        break;
+
+        case 'E':
+        Neill_SDL_SetDrawColour(&sw, 12, 201, 178);
+        rectangle.x = (w*SQH);
+        rectangle.y = (h*SQH);
+        SDL_RenderFillRect(sw.renderer, &rectangle);
+        break;
+
+        case '.':
+        Neill_SDL_SetDrawColour(&sw, 0, 0, 0);
+        rectangle.x = (w*SQH);
+        rectangle.y = (h*SQH);
+        SDL_RenderFillRect(sw.renderer, &rectangle);
+        break;
+
+        default:
+        Neill_SDL_SetDrawColour(&sw, 189, 135, 201);
+        rectangle.x = (w*SQH);
+        rectangle.y = (h*SQH);
+        SDL_RenderFillRect(sw.renderer, &rectangle);
+        break;
+      }
+    }
+  }
+  // Updates window - no graphics appear on some devices until this is finished
+  SDL_RenderPresent(sw.renderer);
+  SDL_UpdateWindowSurface(sw.win);
+  // Checks if anyone has triggered the SDL window to end
+  Neill_SDL_Events(&sw);
+  }while(!sw.finished);
+
+    Neill_SDL_SetDrawColour(&sw, 0, 0, 0);
     SDL_RenderClear(sw.renderer);
     Neill_SDL_Events(&sw);
 
-	SDL_Delay(100);
+  SDL_Delay(100);
 
-	return 0;
+  return 0;
 }
 
 // void clean(){
