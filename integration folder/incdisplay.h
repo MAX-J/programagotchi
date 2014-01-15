68eb55ebdae94470e826917b670e1f24d01a8204
andreasyiannakou <andreasyiannakou92@googlemail.com>
2014-01-15 16:46:04 +0000

    reuploaded incdisplay files

---
+++ integration folder/incdisplay.h
@@ -0,0 +1,19 @@
+// Display graphics using SDL
+#include <SDL.h>
+
+// to move foreground image over background
+struct display;
+
+typedef struct display Display;
+// Create a display window
+Display *start(int w, int h, char *bg, char *fg);
+// Paint scene, handle events, delay for 1/50 second
+void paint(Display *d, int x, int y);
+
+void MAXJ_SDL_DrawText(Display *d, const char* text, int x, int y);
+
+void Incubator(Display *d);
+
+void Menu(Display *d);
+
+void SubMenu(Display *d, int level);
