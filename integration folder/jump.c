4039435592045d09c1ddb7b47e52f59426976f6f
yayayang <kimmy.yang12@gmail.com>
2014-01-16 11:15:10 +0000

    Update jump.c

--- integration folder/jump.c
+++ integration folder/jump.c
@@ -15,7 +15,7 @@ int playJump(char gamearray[HEIGHT][WIDTH], SDL_Simplewin gamewin){
 	  		EXIT;
        		 }
         	else if(command == 'G'){
-	  		SDL(gamearray, "Type in next move",NO_SCORE,gamewin);
+	  		SDL(gamearray, "Type in next move, try to get candies ",NO_SCORE,gamewin);
 		}
 	
 	do{
@@ -29,12 +29,13 @@ int playJump(char gamearray[HEIGHT][WIDTH], SDL_Simplewin gamewin){
 	        	}
 	       		else{
 	      			state = runcommand(gamewin,gamearray,str);
+	      			printf("%d",state);
 	      			if(state == ATE_CANDY){
-					SDL(gamearray,"You got candy",score+=10,gamewin);
+					SDL(gamearray,"You got candy,you need 5 candies to win",score+=10,gamewin);
 					state = NO_ACTION;
 	      			}
 	      			if(score == 50){
-					SDL(gamearray, "Level Completed. Congratulations", NO_SCORE, gamewin);
+					SDL(gamearray, "Level Completed. Congralutations", NO_SCORE, gamewin);
 					SDL_Delay(1000);
 					return WIN;
 	      			}
@@ -47,9 +48,11 @@ int playJump(char gamearray[HEIGHT][WIDTH], SDL_Simplewin gamewin){
 					SDL(gamearray, "Exiting Game", NO_SCORE, gamewin);
 					SDL_Delay(1000);
 	        			return LOSE;
-	} 
+				} 
+			
 	    		}  
 	}while(state != BAD_COMMAND);
+
 	if(state == QUIT_COMMAND){
 		SDL(gamearray, "Exiting Game", NO_SCORE, gamewin);
 		SDL_Delay(1000);
@@ -67,8 +70,8 @@ int playJump(char gamearray[HEIGHT][WIDTH], SDL_Simplewin gamewin){
 		SDL_Delay(1000);
 	        return WIN;
 	}
-	   // SDL(gamearray, "",  NO_SCORE,gamewin);
-	SDL(gamearray, "Type in next move",NO_SCORE,gamewin);
+	  
+	SDL(gamearray, "Type in next move,try to get candies",NO_SCORE,gamewin);
 	Neill_SDL_Events(&gamewin);
 	    
 	}while(state == NO_ACTION || !gamewin.finished);
@@ -77,54 +80,3 @@ int playJump(char gamearray[HEIGHT][WIDTH], SDL_Simplewin gamewin){
 }	
 
 
-/*int gamestate(char gamearray[HEIGHT][WIDTH],int score,int rowshift,int colshift,SDL_Simplewin gamewin) {
-
-        char laststatus = '.';
-	int row = 0;
-	int col = 0;
-        int newrow = row + rowshift;
-        int newcol = col + colshift;
-        
-        if( newrow < HEIGHT &&  newcol < WIDTH 
-        && newrow >= 1 &&  newcol >= 1)
-        {
-                if(gamearray[newrow][newcol] == 'C')
-                {
-                        score += 10;
-                        gamearray[newrow][newcol] = 'G';
-                        gamearray[row][col] = laststatus;
-                        SDL(gamearray,"You get a candy", NO_SCORE,gamewin);
-                }
-                else if(gamearray[newrow][newcol] == '1' ||
-                        gamearray[newrow][newcol] == '#')
-                {
-                        SDL(gamearray,"Ouch!you meet a barrier!", NO_SCORE,gamewin);
-                        SDL(gamearray,"Please input a valid value!", NO_SCORE,gamewin);
-                        return 1;
-                }
-		else if(gamearray[newrow][newcol] == 'X')
-		{
-			SDL(gamearray,"oh my god!you jump to a hole!", NO_SCORE,gamewin);
-			return -1;
-		}
-                else if(gamearray[newrow][newcol] == '.')
-                {
-                        gamearray[row][col] = laststatus;
-                        laststatus = '.';
-                        gamearray[newrow][newcol] = 'G';
-                }
-                else
-                {
-                        gamearray[row][col] = laststatus;
-                        laststatus = gamearray[newrow][newcol];
-                        gamearray[newrow][newcol] = 'G';
-                }
-                row = newrow;
-                col = newcol;
-        }
-        else
-        {
-                SDL(gamearray,"Oops!you are trying to jump out of the wall!", NO_SCORE,gamewin);
-        }
-        return 0;
-}*/
