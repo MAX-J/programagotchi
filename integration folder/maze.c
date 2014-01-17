bfce05965f8b630728371d2c2817d14caa1909bb
Marc Pinto <m.r.pinto@outlook.com>
2014-01-17 14:15:28 +0000

    bugs fixed

--- integration folder/maze.c
+++ integration folder/maze.c
@@ -5,10 +5,14 @@
 #define GORIGHT 1
 #define CHANGEDIRECTION (direction+ 1)%2
 #define COMPLETED 0
+#define DEATH SDL(gamearray, "You died",  NO_SCORE, 300, gamewin)
+#define ERROR(PHRASE) {fprintf(stderr,"Fatal Error %s occured in %s, line %d\n", PHRASE, __FILE__, __LINE__); exit(2); }
 
 int moveBaddies(char gamearray[HEIGHT][WIDTH], int direction, SDL_Simplewin gamewin);
 
 int moveHorizontally(char gamearray[HEIGHT][WIDTH], int i, int j, int direction, SDL_Simplewin gamewin);
+
+void displayDeathscreen(char gamearray[HEIGHT][WIDTH],  SDL_Simplewin gamewin);
 
 int playMaze(char gamearray[HEIGHT][WIDTH], SDL_Simplewin gamewin){
 	
@@ -16,7 +20,7 @@ int playMaze(char gamearray[HEIGHT][WIDTH], SDL_Simplewin gamewin){
 	int state = 0;
 	char str[STRLEN];
 	
-	SDL(gamearray, "Welcome to Maze City.\nTry moving your Gotchi by using the 'move' command.\n Feeling more adventurous, try editing the findCandy.gfn", NO_SCORE, 5000, gamewin);
+	SDL(gamearray, "      Welcome to Maze City.\nTry moving your Gotchi by using the 'move' command.\n Feeling more adventurous, try editing the findCandy.gfn", NO_SCORE, 3000, gamewin);
 	
 	//SDL(gamearray, "Try moving your Gotchi by using the 'move' command", NO_SCORE, gamewin);
 	  
@@ -32,52 +36,56 @@ int playMaze(char gamearray[HEIGHT][WIDTH], SDL_Simplewin gamewin){
 
 			else{
 				state = runcommand(gamewin, gamearray, str);
-			
+				
 				if(state == ATE_CANDY){
-				SDL(gamearray, "You got candy",score++, 1000, gamewin);			
-				state = NO_ACTION;
-			}
+					SDL(gamearray, "You got candy",score++, 1000, gamewin);			
+					state = NO_ACTION;
+				}
 
-			if(state == ON_EXIT){
-				SDL(gamearray, "Level Completed. Congratulations",score, 1000, gamewin);
-				return WIN;
-			}
+				if(state == ON_EXIT){
+					SDL(gamearray, "Level Completed. Congratulations",score, 1000, gamewin);
+					return WIN;
+				}
 
-			else if(state == ON_HAZARD){
-				SDL(gamearray, "You died",  score, 1000, gamewin);
-				return LOSE;
-			}
+				else if(state == ON_HAZARD){
+					DEATH;
+					displayDeathscreen(gamearray, gamewin);
+					return LOSE;
+				}
 			}
 
 		}while(state == BAD_COMMAND);
 		if(firstmove){
-			SDL(gamearray, "You did it. Congratulations",score, 2000, gamewin);
+			SDL(gamearray, "You did it. Congratulations",score, 200, gamewin);
 			firstmove = COMPLETED;		
 		}		
 
 		if(state == QUIT_COMMAND){
-			SDL(gamearray, "Exiting Game",NO_SCORE, 1000, gamewin);
+			SDL(gamearray, "Exiting Game",NO_SCORE, 300, gamewin);
 			return LOSE;
 		}
 		
 		else if(state == ON_EXIT){
-			SDL(gamearray, "Level Completed. Congratulations",score,1000, gamewin);
+			SDL(gamearray, "Level Completed. Congratulations",score,300, gamewin);
 			return WIN;
 		}
 		
 		else if(state == ON_HAZARD){
-			SDL(gamearray, "You died",NO_SCORE, 1000,gamewin);
+			DEATH;
+			displayDeathscreen(gamearray, gamewin);
 			return LOSE;
 		}
 		
 		if(direction == 0 || direction == 1){
 			direction = moveBaddies(gamearray, direction, gamewin);
+			printf("\n");
 		}
 		
 		if(direction == ON_HAZARD){
-			SDL(gamearray, "You died", NO_SCORE, 1000, gamewin);
+			DEATH;
+			displayDeathscreen(gamearray, gamewin);
 			return LOSE;
-		}
+		}		
 		
 		SDL(gamearray, "Type in next move", score, 1000,gamewin);
 		
@@ -98,7 +106,7 @@ int moveBaddies(char gamearray[HEIGHT][WIDTH], int direction, SDL_Simplewin game
 				j = moveHorizontally(gamearray, i , j, direction, gamewin);
 				i++;
 			}
-			if( j < 0){
+			if(j < 0){
 				return ON_HAZARD;
 			}
 		}
@@ -113,7 +121,7 @@ int moveHorizontally(char gamearray[HEIGHT][WIDTH], int i, int j, int direction,
 		if((gamearray[i][j - 1] == GOTCHI || gamearray[i][j - 1] == FREE_SPACE) && direction == GOLEFT){
 			do{
 				if(gamearray[i][j - 1] == GOTCHI){
-					return -5; 
+					return ON_HAZARD; 
 				}
 				if(gamearray[i][j - 1] != FREE_SPACE){
 					return j;
@@ -129,7 +137,7 @@ int moveHorizontally(char gamearray[HEIGHT][WIDTH], int i, int j, int direction,
 	else if((gamearray[i][j + 1] == GOTCHI || gamearray[i][j + 1] == FREE_SPACE) && direction == GORIGHT){
 		do{
 			if(gamearray[i][j + 1] == GOTCHI){
-				return -5; 
+				return ON_HAZARD; 
 			}
 			if(gamearray[i][j + 1] != FREE_SPACE){
 				return j;
@@ -145,4 +153,18 @@ int moveHorizontally(char gamearray[HEIGHT][WIDTH], int i, int j, int direction,
 
 	return j;
 }
+
+void displayDeathscreen(char gamearray[HEIGHT][WIDTH],  SDL_Simplewin gamewin)
+{
+    int m, n;
+    for (m = 0; m < HEIGHT; m++) {
+		for (n = 0; n < WIDTH; n++) {
+	    	gamearray[m][n] = HAZARD;
+			
+		}
+		SDL(gamearray, "You died",  NO_SCORE, 0, gamewin);	
+    }
+	SDL_Delay(500);
+}
+
 				
