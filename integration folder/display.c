1abb2f637750f10a743c31eb0a80e838cd18467b
JosephTalbot <jt13692@my.bris.ac.uk>
2014-01-17 16:39:38 +0000

    Purple colour
    specific RGB values for John’s favourite colour

--- integration folder/display.c
+++ integration folder/display.c
@@ -60,7 +60,7 @@ int SDL(char board[][WIDTH], char string[MAX], int score, int delay, SDL_Simplew
         */
 
         case '1':
-        Neill_SDL_SetDrawColour(&sw, 106,90,205);
+        Neill_SDL_SetDrawColour(&sw, 134, 24, 123);
         rectangle.x = (w*SQH);
         rectangle.y = (h*SQH+OFF);
         SDL_RenderDrawRect(sw.renderer, &rectangle);
