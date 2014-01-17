54b1849d7d213518ffb6c090f352908aea68aaac
Marc Pinto <m.r.pinto@outlook.com>
2014-01-17 16:03:10 +0000

    win screen function added

--- integration folder/maze.h
+++ integration folder/maze.h
@@ -1,1 +1,3 @@
 int playMaze(char gamearray[HEIGHT][WIDTH], SDL_Simplewin sw);
+void displayDeathscreen(char gamearray[HEIGHT][WIDTH],  SDL_Simplewin gamewin);
+void displayWinscreen(char gamearray[HEIGHT][WIDTH],  SDL_Simplewin gamewin);
